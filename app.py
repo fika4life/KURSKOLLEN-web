fa23daec7dfa9d1f1d85778ba0902636963a330d
fika4life <fistewie@gmail.com>
2021-06-25 21:22:14 +0200

    git init

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+   
+
+
+@app.route('/login')
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
---
+++ static/script.js
@@ -0,0 +1,18 @@
+// const registerForm = document.getElementById("registerForm");
+
+// const email = document.getElementById("email");
+// const password = document.getElementById("password");
+
+// // show error message
+
+// // show success outline
+
+// // check all fields filled in
+
+// //check email is correct format
+
+// //check password is at least 8 characters
+
+// registerForm.addEventListener("submit", function (e) {
+//   e.preventDefault();
+// });
